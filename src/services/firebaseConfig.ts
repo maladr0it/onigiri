fb16782d856f372af67bbb52e2099be652fb4c6f
maladr0it <bradford.lachlan@gmail.com>
2020-02-21 15:10:26 +0900

    temporarily add firebaseConfig

---
+++ src/services/firebaseConfig.ts
@@ -0,0 +1,19 @@
+// export default {
+//   apiKey: "AIzaSyCJ7UMVKv0m2rLA1GpFB_K1fqpVr8lqckU",
+//   authDomain: "onigiri-dfec0.firebaseapp.com",
+//   databaseURL: "https://onigiri-dfec0.firebaseio.com",
+//   projectId: "onigiri-dfec0",
+//   storageBucket: "onigiri-dfec0.appspot.com",
+//   messagingSenderId: "1035969864447",
+//   appId: "1:1035969864447:web:48099a6416cbee11bbe2ca",
+// };
+
+export default {
+  apiKey: "AIzaSyD4VF5hhvlzpx-OwHXF6pkuWP-qpH4QSNw",
+  authDomain: "onigiri-7fc74.firebaseapp.com",
+  databaseURL: "https://onigiri-7fc74.firebaseio.com",
+  projectId: "onigiri-7fc74",
+  storageBucket: "onigiri-7fc74.appspot.com",
+  messagingSenderId: "132206882562",
+  appId: "1:132206882562:web:e35920bed90817112adc1b",
+};
